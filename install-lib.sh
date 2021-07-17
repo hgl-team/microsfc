acfd30cdadfc410bdea1428dac7130253e97391f
Leonardo Huerfano <huerfano.jerson@gmail.com>
2021-07-17 15:27:47 -0500

    Added install lib script

---
+++ install-lib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf ~/Arduino/libraries/microsfc
+mkdir -p ~/Arduino/libraries/microsfc
+cp -r ../src/* ~/Arduino/libraries/microsfc/
+cp -r ../include/* ~/Arduino/libraries/microsfc/
