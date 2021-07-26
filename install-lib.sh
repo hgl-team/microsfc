c6fdbe7eb3d820e4f2bc6efe47038fae75ce3669
Leonardo Huerfano <huerfano.jerson@gmail.com>
2021-07-25 22:20:51 -0500

    Changed install script

--- install-lib.sh
+++ install-lib.sh
@@ -2,5 +2,5 @@
 
 rm -rf ~/Arduino/libraries/microsfc
 mkdir -p ~/Arduino/libraries/microsfc
-cp -r ../src/* ~/Arduino/libraries/microsfc/
-cp -r ../include/* ~/Arduino/libraries/microsfc/
+cp -r src/* ~/Arduino/libraries/microsfc/
+cp -r include/* ~/Arduino/libraries/microsfc/
